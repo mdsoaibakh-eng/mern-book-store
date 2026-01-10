55d6810bb65f28eb33ce5d198782cc57c0e64db7
mdsoaibakh-eng <mdsoaibakh>
2026-01-10 18:35:40 +0530

    hello world

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { PORT } from './config.js';
 
 const app = express();
 
+app.get('/', (request, response) => {
+    console.log('Received a request');
+    return response.status(200).send('Hello World!');
+});
+
 app.listen(PORT, () => {
     console.log(`App listening to port ${PORT}`);
 });
