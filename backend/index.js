eb2f49840d1f816b236a3bcd8718eb7b7602ea5c
mdsoaibakh-eng <mdsoaibakh>
2026-01-11 12:30:11 +0530

    Fix backend repo and track backend source code

---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import { PORT, mongoDBURL} from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
+const app = express();
+
+// Middleware for parsing request body
+app.use(express.json());
+
+// Middleware for handling CORS POLICY
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type'],
+  })
+);
+
+// Test route
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
+});
+
+// Routes
+app.use('/api/books', booksRoute);
+
+// MongoDB connection
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
---
+++ backend/models/bookModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+  publishedDate: { type: Date, required: true },
+  pages: { type: Number, required: true }
+});
+
+const Book = mongoose.model("Book", bookSchema);
+export default Book;
